6f55fb274e76b49ecee1ef45a7929aa4e51b68aa
Filip Kniaś <filip.knias@o2.pl>
2025-03-09 11:55:12 +0100

    add config for query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from '@/pages/Home';
+import MovieDetails from '@/pages/MovieDetails';
 import { Provider } from "@/components/ui/provider.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Container, Theme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import MovieDetails from '@/pages/MovieDetails';
+import { queryClientConfig } from '@/config/queryClient';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 60 * 24,
-    },
-  }
-});
+const queryClient = new QueryClient(queryClientConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
